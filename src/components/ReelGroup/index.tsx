720da158259c37b4fa1e8bba6c75a30b5ef88162
AlexKole113 <alexander.koledov@gmail.com>
2021-06-25 19:26:58 +0200

    add project

--- src/components/ReelGroup/index.tsx
+++ src/components/ReelGroup/index.tsx
@@ -5,19 +5,20 @@ import styles from './styles/index.css'
 
 const ReelGroup = ({win,slots}:{win:string|null,slots:string[]}) => {
 
+    const calculateTranslate     = (number:number) =>  100 - ( (100 / (slots.length * 3) ) * ( number + 1 ) );
+    const calculateStartPosition = (number:number) => (100 / (slots.length * 3) ) * number;
+
     const winsMap = new Map([]);
     slots.forEach(( item, number) => {
-        winsMap.set( item, 100 - ( (100/ (slots.length * 3) ) * ( number + 1 ) ) )
+        winsMap.set( item, calculateTranslate( number ) )
     })
 
 
-    const translateTo = ( win ) ? winsMap.get( win ) : 0;
-
     return (
         <div className={styles.reelGroup}>
-            <ReelItem slots={slots} translateTo={ ( typeof translateTo === 'number' ) ? translateTo : 0 } delay={0} />
-            <ReelItem slots={slots} translateTo={ ( typeof translateTo === 'number' ) ? translateTo : 0 } delay={2} />
-            <ReelItem slots={slots} translateTo={ ( typeof translateTo === 'number' ) ? translateTo : 0 } delay={4} />
+            <ReelItem slots={slots} translateTo={ ( win ) ? winsMap.get( win ) : calculateStartPosition(0)} delay={0} />
+            <ReelItem slots={slots} translateTo={ ( win ) ? winsMap.get( win ) : calculateStartPosition(1)} delay={2} />
+            <ReelItem slots={slots} translateTo={ ( win ) ? winsMap.get( win ) : calculateStartPosition(2)} delay={4} />
         </div>
     )
 }
--- src/components/ReelItem/index.tsx
+++ src/components/ReelItem/index.tsx
@@ -2,12 +2,12 @@ import ReelItemSlotsGroup from "@/components/ReelItemSlotsGroup";
 
 import styles from './styles/index.css'
 
-const ReelItem = ({slots, translateTo, delay }:{slots:string[], translateTo: number, delay:number}) => {
+const ReelItem = ({slots, translateTo, delay }:{slots:string[], translateTo: number|unknown, delay:number}) => {
 
     // cubic-bezier(1, 1.39, 0.82, 0.9)
     return(
         <div className={styles.slot}>
-            <div style={{transform: `translateY(${translateTo}%)`, transition: `transform .8s .${delay}s cubic-bezier(1, 1.36, 0.85, 0.93)` }} className={styles.reelItem}>
+            <div style={{transform: `translateY(${ (typeof translateTo === 'number') ? translateTo : 0 }%)`, transition: `transform .8s .${delay}s cubic-bezier(1, 1.36, 0.85, 0.93)` }} className={styles.reelItem}>
                 <ReelItemSlotsGroup slots={slots} />
                 <ReelItemSlotsGroup slots={slots} />
                 <ReelItemSlotsGroup slots={slots} />
